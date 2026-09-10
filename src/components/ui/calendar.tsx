044845314d226a891448b0a39e4d2241f0395338
Ashfaq Naseem <ashfaqnaseem1@gmail.com>
2026-09-10 09:30:53 +0600

    fix: ts error

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,14 +1,10 @@
-import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker, type DayPickerProps } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 
-interface CalendarProps extends DayPickerProps {
-  showOutsideDays?: boolean
-}
-
+type CalendarProps = DayPickerProps
 function Calendar({
   className,
   classNames,
@@ -52,8 +48,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
+        IconRight: () => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
